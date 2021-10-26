6fdc0f5f709344257039e4c78c3bb379d7e5267d
Szymon Szyszkowski <ss426665@icm.edu.pl>
2021-10-26 17:26:14 +0200

    added support for multiple entries

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,32 @@ const path = require("path");
 
 module.exports = () => {
   return {
-    mode:
-      process.env.NODE_ENV_MODE === "production" ? "production" : "development",
     /* NODE_ENV_MODE is set in the production script for the production, if not set than  use  developments
      */
-    target: process.env.MODE_ENV_MODE === "production" ? "browserlist" : "web",
+    mode:
+      process.env.NODE_ENV_MODE === "production" ? "production" : "development",
     /* Use web or browserlist: defaults -https://github.com/browserslist/browserslist#best-practices for the configuration as we target webpack for the front of our application */
-    entry: path.resolve(__dirname, "src", "Client-Layer", "index.js"),
-    // output directory, where webpack will bundle our code
+    target: process.env.MODE_ENV_MODE === "production" ? "browserlist" : "web",
+    /*
+        two entry points for the app - one for the background app - CMS like (Admin) and second for front app (Blog)
+    */
+    entry: {
+      Admin: {
+        import: "./src/Client-Layer/Admin-Layer/app.js",
+        filename: "./Client-Layer/Admin-Layer/[name].js",
+      },
+      Blog: {
+        import: "./src/Client-Layer/Blog-Layer/app.js",
+        filename: "./Client-Layer/Blog-Layer/[name].js",
+      },
+    },
+
+    // output directory, where webpack will bundle our code, resolves to Dist/Client-Layer
     output: {
-      filename: "main.js",
-      path: path.resolve(__dirname, "Dist", "Client-Layer"),
-      assetModuleFilename: "Images/[name][ext]",
+      path:
+        process.env.NODE_ENV_MODE === "production"
+          ? path.resolve(__dirname, "Prod")
+          : path.resolve(__dirname, "Dist"),
     },
   };
 };
