0522ed095ccd7ad18f7dbfe422c9bae18b1a728b
Szymon Szyszkowski <ss426665@icm.edu.pl>
2021-10-26 21:39:24 +0200

    minor changes to project setup

---
+++ src/Client-Layer/Admin-Layer/app/app.js
@@ -0,0 +1,4 @@
+import react from "react";
+
+const App = react.createElement("h1", "hello world");
+export default App;
---
+++ src/Client-Layer/Admin-Layer/index.js
@@ -0,0 +1,7 @@
+import reactDOM from "react-dom";
+import react from "react";
+import App from "./app/app";
+
+const root = document.getElementById("root");
+
+reactDOM.render(<App />, root);
